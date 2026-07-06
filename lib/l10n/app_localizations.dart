a51d78664cbe8dd55f2c8e24409227dbc223c750
shahinurislam <shahinalam6644@gmail.com>
2026-07-07 00:27:48 +0600

    without booklet

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -101,7 +101,7 @@ abstract class AppLocalizations {
   /// No description provided for @appName.
   ///
   /// In en, this message translates to:
-  /// **'My App'**
+  /// **'Salam Apps'**
   String get appName;
 
   /// No description provided for @login.
--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -9,7 +9,7 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get appName => 'My App';
+  String get appName => 'Salam Apps';
 
   @override
   String get login => 'Login';
